d206e0028690b384f7adb22465142efc3f202b19
HumairaFatima757 <fatimahumaira757@gmail.com>
2023-04-07 21:00:30 +0500

    JS ass 1

---
+++ ass1.js
@@ -0,0 +1,107 @@
+                  // QUESTION NO 1 ============================================
+
+
+// let num1 = prompt("enter 1st number");
+// let num2 = prompt("enter 2nd  number");
+// if (num1 < num2 )
+// { console.log("greater number is =  "+ num2 );
+
+// }else {console.log ("greater number is =  "+ num1)}
+
+
+                                // QUESTION NO 2 ============================================= 
+
+
+// let num1 = prompt ("enter any number")
+// if (num1>0){console.log(alert("the sign is +"))}
+// else{alert("the sign is -")}
+
+                                 
+
+                                     //   QUESTION NO 3========================================
+
+
+
+
+
+// let num1 = prompt("enter 1st number");
+// let num2 = prompt("enter 2nd  number");
+// let num3 = prompt("enter 3rd number");
+// let num4 = prompt("enter 4th  number");
+// let num5 = prompt("enter 5th number");
+
+// if (num1 < num2 && num3 < num2 && num4 < num2 && num5 < num2  )
+// { console.log("greater number is =  "+ num2 );
+// }
+// else if (num1 < num3 && num2 < num3 && num4 < num3 && num5 < num3 )
+// { console.log("greater number is =  "+ num3 );
+// }
+// else if (num1 < num4 && num2 < num4 && num3 < num4 && num5 < num4 )
+// { console.log("greater number is =  "+ num4 );
+// }
+// else if (num1 < num5 && num2 < num5 && num4 < num5 && num3 < num5 )
+// { console.log("greater number is =  "+ num5 );}
+//  else {console.log ("greater number is =  "+ num1)}
+
+                                
+
+                         // QUESTION NO 4=====================================================
+
+
+
+
+// for(let i =0 ; i <= 15; i++){
+//     // let userInput = prompt("Enter a number ");
+//     if (i===0){console.log (i + "is even" )
+
+//     }else if (i%2 ===0){
+//         console.log(i +" is  even ")
+//     }else{ console.log(i +" is  odd ") }
+    
+// }
+                        //   QUESTION NO 5======================================================
+
+
+// let userInput = prompt ("enter your numbers");
+//  if  (userInput <100 && userInput>=90 ){ console.log  ( "A"  )}
+
+//  else if  (userInput <90  && userInput>=80){ console.log ("B"  )}
+//  else if  (userInput <80  && userInput>=70){ console.log ("C"  )}
+//  else if  (userInput <70  && userInput>=60){ console.log ("D"  )}
+//  else if  (userInput <60  && userInput>=50 ){ console.log ("F"  )}
+//  else {console.log("you are fail")}
+
+
+
+                   //    QUESTION NO 6====================================================== 
+
+
+// for(i = 1; i <= 100; i++)
+//     if (i%3 === 0 && i%5 === 0 )
+//     {console.log("fizzBuzz")}
+//     else if(i%3 === 0) { console.log("fizz")}
+//     else if(i%5 === 0) { console.log("Buzz")}
+//     else {console.log(i)}
+
+
+                //   QUESTION NO 7=============================================================
+
+// let x ;
+// let y;
+// let star ;
+// for (x= 1 ; x <= 6 ; x++){
+//     for (y=1 ; y < x ; y++)
+//     {
+// star = star + ("*");
+
+//     }
+//     console.log (star);
+//     star = " " ;
+// }
+
+
+
+
+
+
+
